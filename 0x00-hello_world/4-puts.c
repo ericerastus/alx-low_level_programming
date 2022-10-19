30d94968728af23cc06c17e415e5fa59bcae23d1
Ric Tugi <113793244+ericerastus@users.noreply.github.com>
2022-10-19 18:09:19 +0300

    Project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the string in the put function
+ *
+ *Description:using the main function
+ *this program prints \"Programming is like building a multilingual puzzle
+ * Return:zero if no errors otherwise returns non-zero value.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
