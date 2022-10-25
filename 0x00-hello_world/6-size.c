b73c6852290dd66f2a1956fbbe05530ca043b5c0
Ric Tugi <113793244+ericerastus@users.noreply.github.com>
2022-10-25 06:58:16 +0300

    Task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char sizeChar;
+	int sizeInt;
+	long sizeLong;
+	long long sizeLongLong;
+	float sizeFloat;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(sizeChar));
+	printf("Size of an int: %zu byte(s)\n", sizeof(sizeInt));
+	printf("Size of long int: %zu byte(s)\n", sizeof(sizeLong));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(sizeLongLong));
+	printf("Size of a float: %zu byte(s)\n", sizeof(sizeFloat));
+}
