61512825c764419d01f63f3b520132d26cebd1be
Ric Tugi <113793244+ericerastus@users.noreply.github.com>
2022-10-25 06:00:31 +0300

    Task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)	/* Prints exactly
 		 * Programming is like building a multilingual puzzle,
 		 * followed by a new line
