92b1eea894cd3e580b586c43778617f61d22fe05
Ric Tugi <113793244+ericerastus@users.noreply.github.com>
2022-11-04 22:31:07 +0300

    Task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	int num = 0;
+	int num;
 
-	while (num < 10)
+	for (num = 0; num < 10; num++)
 	{
 		putchar(num + '0');
+		if (num < 9)
+		{
 		putchar(',');
 		putchar(' ');
-		num++;
+		}
 	}
-	{
-		putchar('\n');
-	}
+	putchar('\n');
 	return (0);
 }
