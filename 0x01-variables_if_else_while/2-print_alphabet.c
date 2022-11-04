ddc8f6538d66ea91394dd170a7d88afbf38d2608
Ric Tugi <113793244+ericerastus@users.noreply.github.com>
2022-11-04 19:25:10 +0300

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+	}
+		return (0);
+}
