c5f11035037a341a3d76a44e9327f21a479c427b
Ric Tugi <113793244+ericerastus@users.noreply.github.com>
2022-11-04 22:55:13 +0300

    Task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,4 +8,19 @@
 
 int main(void)
 {
+	int num;
+	char let;
 
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	for (let = 'a'; let <= 'f'; let++)
+	{
+		putchar(let);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
