35c540b3e0a7d19f5f7f076b59dae0a28b3dbd4d
Ric Tugi <113793244+ericerastus@users.noreply.github.com>
2022-12-01 03:05:36 -0500

    Task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,13 @@
 
 /**
  * _isalpha - checks alphabetic char
+ * @c: represents character
  *
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' && c >+ 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
