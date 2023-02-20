570e163d2a07fb95bd86323015b35ec51b091317
Ric Tugi <mimi@wewe.com>
2023-02-20 21:36:47 +0300

    Task 9 correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,17 @@
 
 int main(void)
 {
-	int num = 0;
+	int num;
 
-	while (num < 9)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
-		putchar(44);
-		putchar(32);
-		num++;
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar(57);
+	putchar('\n');
 	return (0);
 }
