2c8b7d2b7e2c5697930b0f0f04545e71d70bfab7
Ric Tugi <mimi@wewe.com>
2023-02-21 05:56:26 +0300

    Task 10-12

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,17 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		putchar(num1 + '0');
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar(num1 + '0');
 			putchar(num2 + '0');
-		putchar(',');
-		putchar(' ');
+		if (!((num1 == 8) && (num2 == 9)))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
 	}
+	putchar('\n');
 	return (0);
 }
