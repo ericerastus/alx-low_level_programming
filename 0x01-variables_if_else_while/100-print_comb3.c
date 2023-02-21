617a9a12349a6188b4cec06969f0e0aeb48b0d32
Ric Tugi <mimi@wewe.com>
2023-02-21 05:24:10 +0300

    Task 4

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,6 +10,15 @@
 
 int main(void)
 {
+	int num1, num2;
 
-
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		putchar(num1 + '0');
+		for (num2 = 0; num2 < 10; num2++)
+			putchar(num2 + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,18 +3,18 @@
 /**
  * main - print alphabet a-z exclude q&e
  *
- * Description: main function printing alphabet a-z lowercase excluding letters q&e
+ * Description: main function printing
+ * alphabet a-z lowercase excluding letters q&e
  * Return: 0 if successful
  */
 
 int main(void)
 {
-	char alp = 'a';
+	char alp;
 
-	while (alp != 'e' || alp != 'q' && alp <= 'z')
-	{
-		putchar(alp);
-		alp++;
-	}
+	for (alp = 'a'; alp <= 'z'; alp++)
+		if (alp != 'e' && alp != 'q')
+			putchar(alp);
+	putchar('\n');
 	return (0);
 }
