a83c2f85b247109b9ca0dd1b92ccc080571c820d
Ric Tugi <mimi@wewe.com>
2023-02-23 11:53:31 +0300

    Tasks 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: number to add
+ * @b: number to add
+ *
+ * Return: the integer
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ *
+ * Return: no retun value
+ */
+
+void times_table(void)
+{
+	int num
